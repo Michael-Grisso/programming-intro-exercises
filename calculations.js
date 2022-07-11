b4ad1f87c1113c0e9dbed0576f18eedc064ce973
Michael Grisso <79596771+Michael-Grisso@users.noreply.github.com>
2022-07-11 18:31:20 -0400

    Created JS folder
    Visibly unappealing with no folder.  Allowed me to learn a little more about github as well.

--- calculations.js
+++
@@ -1,7 +0,0 @@
-function main(prob1, prob2, prob3) {
-    let result = eval(prob1);
-    let result2 = eval(prob2);
-    let result3 = eval(prob3);
-    console.log(`${result}\n${result2}\n${result3}\n`);
-
-}
